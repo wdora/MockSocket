b6392b923df56482d2ef78c28ede04f944189e98
caolin <caolin@beisen.com>
2023-06-15 20:14:12 +0800

    add benchmark

--- benchmark/MockSocket.Benchmarks/SerializerBencmarks.cs
+++ benchmark/MockSocket.Benchmarks/SerializerBencmarks.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
+
+[MemoryDiagnoser]
 public class SerializerBencmarks
 {
+    private object obj = new DemoUser(1, 3, "Jack");
+
+    [Benchmark]
+    public string SerilizeByNewtonsoft()
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+
+    [Benchmark]
+    public string SerilizeBySystemJson()
+    {
+        return System.Text.Json.JsonSerializer.Serialize(obj);
+    }
 }
+
+record DemoUser(int Id, int Age, string Name);
