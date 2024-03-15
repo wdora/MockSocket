968ff36eeb1ea163f4ce53ddb452b71e425fbf18
caolin <caolin@beisen.com>
2024-03-15 17:12:47 +0800

    add k8s-mocksocket-server

--- build.sh
+++
@@ -1,4 +0,0 @@
-git pull
-docker build -t wdora/mocksocket-server .
-docker rm -f my-mock-server
-docker run -d -e TZ=Asia/Shanghai --restart always --network host --name=my-mock-server wdora/mocksocket-server
